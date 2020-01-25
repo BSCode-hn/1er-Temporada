1c6b3736606ed03460d73d584354b1c69c45e2a5
BSCode <59863452+BSCode-hn@users.noreply.github.com>
2020-01-24 21:46:45 -0500

    Create Queue.py

---
+++ Listas Enlazadas | Linked List/Classes/Queue.py
@@ -0,0 +1,49 @@
+#-*- coding:utf8 -*-
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+
+class LinkedListQueue:
+    def __init__(self):
+        self.first = None
+
+    def add(self,value):
+        if not self.first:
+            self.first = Node(value)
+            return True
+        else:
+            current = self.first
+            while current.next:
+                current = current.next
+            current.next = Node(value)
+            return True
+        return False
+
+    def delete(self):
+        if not self.first:
+            return False
+        else:
+            current = self.first
+            self.first = self.first.next
+            return True
+        return False
+
+    def getFirst(self):
+        return self.first.value
+
+    def getLast(self):
+        current = self.first
+        while(current.next):
+            current = current.next
+        return current.value
+
+    def printLL(self):
+        List = ""
+        current = self.first
+        while(current):
+            List = "%s%s%s" % (List,"-->",current.value)
+            current = current.next
+        List = "%s%s" % (List, "-->null")
+        print(List)
+
